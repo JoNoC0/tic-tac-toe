efb6cb1bca32db5e5f6355bfe83e74242675d2aa
JoNoC0 <jnguyeningco@gmail.com>
2023-04-26 14:24:15 -0500

    add functions and loops for game startup

--- script/app.js
+++ script/app.js
@@ -1,19 +1,55 @@
-// initialize variables
+// Initialize variables
 let currentPlayer = 'X';
 let gameStatus = [];
 let message = document.getElementById('message');
 
-// initialize game board
+// Initialize game board
 for (let i = 0; i < 9; i++) {
+  gameStatus.push('');
+  let square = document.getElementById(i);
+  square.addEventListener('click', () => {
+    if (gameStatus[i] === '') {
+      gameStatus[i] = currentPlayer;
+      square.innerHTML = currentPlayer;
+      square.style.color = currentPlayer === 'X' ? 'red' : 'blue';
+      checkWin();
+      switchPlayer();
+    }
+  });
+}
+
+// Reset game board
+let resetButton = document.getElementById('reset');
+resetButton.addEventListener('click', () => {
+  gameStatus = [];
+  for (let i = 0; i < 9; i++) {
     gameStatus.push('');
     let square = document.getElementById(i);
-    square.addEventListener('click', () => {
-        if (gameStatus[i] === '') {
-            gameStatus[i] = currentPlayer;
-            square.innerHTML = currentPlayer;
-            square.style.color = currentPlayer === 'X' ? 'red' : 'blue';
-            checkWin();
-            switchPlayer();
-        }
-    });
+    square.innerHTML = '';
+    square.style.color = 'black';
+  }
+  message.innerHTML = '';
+  currentPlayer = 'X';
+});
+
+// Check for a win
+function checkWin() {
+  let winCombinations = [    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let combination of winCombinations) {
+    if (
+      gameStatus[combination[0]] === currentPlayer &&
+      gameStatus[combination[1]] === currentPlayer &&
+      gameStatus[combination[2]] === currentPlayer
+    ) {
+      message.innerHTML = `Player ${currentPlayer} wins`
+    }
 }
+};
